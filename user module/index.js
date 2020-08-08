c4ac189b973b85d7584b498a9438036b38095b10
Adenikym <mm5adeniky@gmail.com>
2020-08-08 01:56:59 +0100

    landing page

--- user module/index.js
+++ user module/index.js
@@ -1,1 +1,15 @@
+function addDetails () {
+document.querySelector(".btn3").addEventListener("click", function () {
+    
+    document.querySelector(".bg-modal").style.display="flex"
 
+   
+})
+    
+}
+
+addDetails();
+
+document.querySelector(".sub").addEventListener("click", function() {
+    document.querySelector(".bg-modal").style.display="none";
+})
